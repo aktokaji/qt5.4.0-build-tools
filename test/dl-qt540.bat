44b1691a99e832871b7b270674dc8cd9f6c75a8d
Akio Tokaji <akio.tokaji@gmail.com>
2015-08-12 14:32:58 +0900

    .

---
+++ test/dl-qt540.bat
@@ -0,0 +1,7 @@
+echo on
+mkdir qt-src
+cd qt-src
+rem wget -nv --no-clobber --no-check-certificate https://download.qt.io/archive/qt/5.4/5.4.0/single/qt-everywhere-opensource-src-5.4.0.7z
+wget --no-clobber --no-check-certificate https://download.qt.io/archive/qt/5.4/5.4.0/single/qt-everywhere-opensource-src-5.4.0.7z
+
+pause
