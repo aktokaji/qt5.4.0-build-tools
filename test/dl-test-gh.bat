dc2ce5d2fa4f98038de79b255415f5487a259e5b
Akio Tokaji <akio.tokaji@gmail.com>
2015-08-12 14:06:04 +0900

    .

---
+++ test/dl-test-gh.bat
@@ -0,0 +1,7 @@
+echo on
+mkdir tools
+cd tools
+wget -nv --no-clobber --no-check-certificate -O mingw-qt-5.4.0-as-is.7z https://github.com/aktokaji/qt5.4.0-build-tools/blob/master/mingw-qt-5.4.0-as-is.7z?raw=true
+wget -nv --no-clobber --no-check-certificate -O msys-2015-0114.7z       https://github.com/aktokaji/qt5.4.0-build-tools/blob/master/msys-2015-0114.7z?raw=true
+
+pause
---
+++ test/dl-test-sf.bat
@@ -0,0 +1,8 @@
+echo on
+mkdir tools
+cd tools
+rem wget -nv --no-clobber --no-check-certificate -O mingw-qt-5.4.0-as-is.7z http://jaist.dl.osdn.jp/users/7/7810/mingw-qt-5.4.0-as-is.7z
+wget -nv --no-clobber --no-check-certificate http://jaist.dl.osdn.jp/users/7/7810/mingw-qt-5.4.0-as-is.7z
+wget -nv --no-clobber --no-check-certificate http://jaist.dl.osdn.jp/users/7/7811/msys-2015-0114.7z
+
+pause
